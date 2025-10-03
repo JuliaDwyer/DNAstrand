7039369de22649006acc167180abdba535164819
julia.dwyer <julia.dwyer@MU-C1-12.mullen.pvt.k12.co.us>
2025-10-03 13:52:04 -0600

    Initial Commit

---
+++ DNA Strand/src/DnaStrand.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class DnaStrand
+	{
+
+		public static void main(String[] args)
+			{
+				System.out.println("Please enter a nucleotide sequence.");
+				Scanner nucleotide = new Scanner(System.in);
+				String sequence = nucleotide.nextLine();
+				String [] dnaStrand = new int[nucleotide.length-1]
+				
+				
+			
+				if (sequence[])
+
+			}
+
+	}
